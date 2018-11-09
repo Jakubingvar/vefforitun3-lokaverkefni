fe4f3b8d3e0fefe3f9b535f4346ebd036b684a4e
Jakub Ingvar Pitak <FS596306@fss.is>
2018-11-09 08:49:08 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Lokaverkefni</h1>
+      <p>Jakub Ingvar Pitak</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+
+</style>
